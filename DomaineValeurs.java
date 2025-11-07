40ff5a8e220547e5e83d8a81705ce108e8083528
Soufiane Derouich <Soufiane.derouich@polytech-lille.net>
2025-11-07 11:41:13 +0100

    DomaineValeurs class

--- DomaineValeurs.java
+++ DomaineValeurs.java
@@ -1,8 +1,37 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Représente le domaine de valeurs autorisé pour une caractéristique.
+ */
 public class DomaineValeurs {
     private Double min;
     private Double max;
-    private Set<String> symbols;
+
+    private Set<String> elements;
+    
+    public DomaineValeurs(Double min, Double max) {
+        this.min = min;
+        this.max = max;
+        this.elements = null; 
+    }
+
+    public DomaineValeurs(Set<String> elements) {
+        this.min = null;
+        this.max = null;
+        this.elements = new HashSet<>(elements);
+    }
+
+    public Double getMin() {
+        return min;
+    }
+
+    public Double getMax() {
+        return max;
+    }
+
+    public Set<String> getElements() {
+        return elements;
+    }
+
 }
