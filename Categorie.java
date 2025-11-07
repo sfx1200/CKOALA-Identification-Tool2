5e6158bc4d050ba961707ddddc45be6464f0d10b
Soufiane Derouich <Soufiane.derouich@polytech-lille.net>
2025-11-07 10:43:35 +0100

    categorie class v1

--- Categorie.java
+++ Categorie.java
@@ -1,10 +1,37 @@
-public class categories  {
-    private Map<String,DomaineValeurs> caracteristiques;
-    private String nom;
-    private List<Categorie> fils ;
-    private Categories mere ;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    public Categorie(){
-        this.nom = nom ;
+
+public class Categorie { 
+    private String nom;
+    private Categorie mere;
+    private List<Categorie> fils;
+    
+    private Map<String, DomaineValeurs> caracteristiques;
+
+    public Categorie(String nom) {
+        this.nom = nom;
+        this.mere = null; 
+        this.fils = new ArrayList<>();
+        this.caracteristiques = new HashMap<>();
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Categorie getMere() {
+        return mere;
+    }
+
+    public List<Categorie> getFils() {
+        return fils;
+    }
+
+    public Map<String, DomaineValeurs> getCaracteristiques() {
+        return caracteristiques;
+    }
+
 }
